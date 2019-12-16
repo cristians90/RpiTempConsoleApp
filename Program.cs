2e53fb496c62fae64ae4205ef948d755b22c06ae
cristians90 <cristian.saldias.90@outlook.com>
2019-12-16 00:11:16 -0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+using Iot.Device.CpuTemperature;
+
+namespace RpiTempConsoleApp
+{
+    class Program
+    {
+        static CpuTemperature temperature = new CpuTemperature();
+        static GpioController controller = new GpioController();
+
+        static void Main(string[] args)
+        {
+            controller.OpenPin(22, PinMode.Output);
+
+            while (true)
+            {
+                if (temperature.IsAvailable)
+                {
+                    Console.WriteLine($"The CPU temperature is {temperature.Temperature.Celsius}");
+                }
+
+                controller.Write(22, PinValue.High);
+
+                Thread.Sleep(2000);
+
+                controller.Write(22, PinValue.Low);
+
+                Thread.Sleep(2000);
+            }
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # RpiTempConsoleApp
 Test application for RPI with .Net Core 3.1
+Based in: https://dev.to/azure/net-core-iot-raspberry-pi-linux-and-azure-iot-hub-learn-how-to-build-deploy-and-debug-d1f
